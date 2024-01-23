b7426eb3d5f25df71e0635b281f9ea185ccfc409
matt <matducrest@gmail.com>
2024-01-23 18:23:31 +0100

    Add wind speed to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,5 @@
 const request = require('request')
 
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
 const forecast = (latitude, longitude, callback) => {
     const url = "http://api.weatherstack.com/current?access_key=8431090c09ffff5dd3bf60625ee107de&query=" + latitude + "," + longitude + "&units=m"
     
@@ -20,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.' + ' Current wind speed is ' + body.current.wind_speed)
         }
     })
 }
